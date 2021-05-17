26592c1ed0b6c81507cba23bcad65ddebb8a82b2
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-17 00:50:44 +0000

    FXED | Semistandard style

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,27 +1,26 @@
-$(document).ready(function() {
-    dict_amenities = {}
-    $('input[type=checkbox]').change(function() {
-        if ($(this).is(':checked')) {
-            dict_amenities[$(this).data('id')] = $(this).data('name');
-        }
-        else {
-            delete dict_amenities[$(this).data('id')];
-        }
-        list_amenities = Object.values(dict_amenities);
-        if (list_amenities.length > 0) {
-            const amenities = list_amenities.join(", ");
-            $("DIV.amenities > h4").text(amenities);
-        } else {
-            $("DIV.amenities > h4").html("&nbsp;");
-        }
-    });
+$(document).ready(function () {
+  const dictAmenities = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).is(':checked')) {
+      dictAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete dictAmenities[$(this).data('id')];
+    }
+    const listAmenities = Object.values(dictAmenities);
+    if (listAmenities.length > 0) {
+      const amenities = listAmenities.join(', ');
+      $('DIV.amenities > h4').text(amenities);
+    } else {
+      $('DIV.amenities > h4').html('&nbsp;');
+    }
+  });
 
-    const url = 'http://localhost:5001/api/v1/status/'
-    $.getJSON(url, function(data) {
-        if(data.status === "OK") {
-            $("DIV#api_status").addClass("available");
-        } else {
-            $("DIV#api_status").removeClass("available");
-        }
-    });
+  const url = 'http://localhost:5001/api/v1/status/';
+  $.getJSON(url, function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
 });
